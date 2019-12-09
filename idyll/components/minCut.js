33361db30538967134617f558e3a52a124147d0d
Eshan Tarneja <eshantarneja@github.com>
2019-12-08 22:17:10 -0600

    eshan's big update

--- idyll/components/minCut.js
+++ idyll/components/minCut.js
@@ -4,7 +4,7 @@ const d3 = require('d3');
 
 const size = 500;
 
-class NewD3Component extends D3Component {
+class minCut extends D3Component {
   initialize(node, props) {
     const svg = (this.svg = d3.select(node).append('svg'));
 
@@ -25,31 +25,31 @@ class NewD3Component extends D3Component {
 
 
     var nodes = [
-              { x:   width*.1, y: height*.50, id: 0, text: "S"},
+              { x:   width*.00, y: height*.50, id: 0, text: "S"},
               { x:   width*.35, y: height*.35, id: 1, text: "A"},
               { x:   width*.65, y: height*.35, id: 2, text: "B"},
               { x:   width*.35, y: height*.65, id: 3, text: "C"},
               { x:   width*.65, y: height*.65, id: 4, text: "D"},
-              { x:   width*.9, y: height*.50, id: 5, text: "T"},
+              { x:   width*1.0, y: height*.50, id: 5, text: "T"},
     ];
 
     var links = [
-              { source: 0, target: 1, capacity: 9, id: 0, flow: 0},
-              { source: 0, target: 3, capacity: 11, id: 1, flow: 0},
-              { source: 1, target: 2, capacity: 8, id: 2, flow: 0},
-              { source: 1, target: 3, capacity: 5, id: 3, flow: 0},
-              { source: 2, target: 5, capacity: 9, id: 4, flow: 0},
-              { source: 3, target: 4, capacity: 9, id: 5, flow: 0},
-              { source: 4, target: 2, capacity: 9, id: 6, flow: 0},
-              { source: 4, target: 5, capacity: 8, id: 7, flow: 0},
+              { source: 0, target: 1, capacity: 10, id: 0, flow: 0},
+              { source: 0, target: 3, capacity: 8, id: 1, flow: 0},
+              { source: 1, target: 2, capacity: 5, id: 2, flow: 0},
+              { source: 1, target: 3, capacity: 2, id: 3, flow: 0},
+              { source: 2, target: 5, capacity: 7, id: 4, flow: 0},
+              { source: 3, target: 4, capacity: 10, id: 5, flow: 0},
+              { source: 4, target: 2, capacity: 8, id: 6, flow: 0},
+              { source: 4, target: 5, capacity: 10, id: 7, flow: 0},
     ];
 
     var cuts = [
-    {x1: .25,  y1: .2, x2: .25, y2: .8, id: 0, text: 20, max: 20},
-    {x1: .15,  y1: .3, x2: .55, y2: .75, id: 1, text: 23, max: 20},
-    {x1: .5,  y1: .2, x2: .5, y2: .8, id: 2, text: 17, max: 17},
-    {x1: .45,  y1: .3, x2: .85, y2: .65, id: 3, text: 25, max: 17},
-    {x1: .8,  y1: .2, x2: .8, y2: .8, id: 4, text: 17, max: 17}
+    {x1: .25,  y1: .2, x2: .25, y2: .8, id: 0, text: 18, max: 18},
+    {x1: .15,  y1: .3, x2: .55, y2: .75, id: 1, text: 22, max: 18},
+    {x1: .5,  y1: .2, x2: .5, y2: .8, id: 2, text: 15, max: 15},
+    {x1: .45,  y1: .3, x2: .85, y2: .65, id: 3, text: 23, max: 15},
+    {x1: .8,  y1: .2, x2: .8, y2: .8, id: 4, text: 17, max: 15}
     ];
 
 
@@ -111,7 +111,7 @@ class NewD3Component extends D3Component {
     .attr("id", function(d) {return "movingCapacity"+d.id;})
     .text(function(d){return d.capacity})
     .attr("font-size", "20px")
-    .attr("fill","red")
+    .attr("fill","blue")
     .attr("class","movingCapacity")
     // .attr("opacity","0")
 
@@ -128,11 +128,11 @@ class NewD3Component extends D3Component {
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity0"})
-    .attr("transform", "translate(-100,72) rotate(-25)")
+    .attr("transform", "translate(-100,62) rotate(-25)")
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity1"})
-    .attr("transform", "translate(115,-35) rotate(25)")
+    .attr("transform", "translate(115,-25) rotate(25)")
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity2"})
@@ -140,11 +140,11 @@ class NewD3Component extends D3Component {
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity3"})
-    .attr("transform", "translate(2,0) rotate(0)")
+    .attr("transform", "translate(10,0) rotate(0)")
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity4"})
-    .attr("transform", "translate(115,-155) rotate(25)")
+    .attr("transform", "translate(115,-165) rotate(25)")
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity5"})
@@ -152,11 +152,11 @@ class NewD3Component extends D3Component {
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity6"})
-    .attr("transform", "translate(5,0) rotate(0)")
+    .attr("transform", "translate(10,0) rotate(0)")
 
     this.svg
     .selectAll("text").filter(function(d) {return this.id == "movingCapacity7"})
-    .attr("transform", "translate(-100,195) rotate(-25)")
+    .attr("transform", "translate(-100,205) rotate(-25)")
 
 
     svg.selectAll()
@@ -205,7 +205,7 @@ class NewD3Component extends D3Component {
     .attr("y",50)
     .attr("opacity",0)
     .attr("fill","green")
-    .text("Min Cut = Max Flow = 17")
+    .text("Min Cut = Max Flow = 15")
     .attr("font-size", "30px");
   }
   // each "step" in the idyll file has a number associated with it.
@@ -214,10 +214,7 @@ class NewD3Component extends D3Component {
  update(props, oldProps) {
 
     // console.log("props.myVar")
-    if (props.myVar>0){
-      this.cutAnimation(props, oldProps)
-    }
-    
+    this.cutAnimation(props, oldProps)
     
   }
 
@@ -290,4 +287,4 @@ class NewD3Component extends D3Component {
   }
 }
 
-module.exports = NewD3Component;
+module.exports = minCut;
